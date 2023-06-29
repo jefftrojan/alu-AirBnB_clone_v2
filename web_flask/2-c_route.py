7c0c4e44c8fe6d11f2d516b929991cd38820ebec
jefftrojan <j.dauda@alustudent.com>
2023-06-29 03:47:40 +0200

    2-c_route.py

--- web_flask/2-c_route.py
+++ web_flask/2-c_route.py
@@ -18,7 +18,8 @@ def hbnb():
 
 @app.route('/c/<text>', strict_slashes=False)
 def c_is_fun(text):
-    """ Function that displays "C" followed by the value of the text variable """
+    """ Function that displays "C" followed 
+    by the value of the text variable """
     return 'C {}'.format(text.replace('_', ' '))
 
 if __name__ == '__main__':
