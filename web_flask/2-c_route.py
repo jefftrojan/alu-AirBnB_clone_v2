a6997e6818da0de7787436b6e8f1befb33ed291e
jefftrojan <j.dauda@alustudent.com>
2023-06-29 03:46:31 +0200

    2-c_route.py

--- web_flask/2-c_route.py
+++ web_flask/2-c_route.py
@@ -15,5 +15,11 @@ def hbnb():
     """ Function that displays "HBNB" """
     return 'HBNB'
 
+
+@app.route('/c/<text>', strict_slashes=False)
+def c_is_fun(text):
+    """ Function that displays "C" followed by the value of the text variable """
+    return 'C {}'.format(text.replace('_', ' '))
+
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
